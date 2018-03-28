1ce75c9c9111351722b7de5f4617067845bcf6f9
Sumit Srivastava <ssrivas8@ncsu.edu>
2018-03-27 23:21:20 -0400

    tabbed view working

--- v3/procApp/platforms/android/app/src/main/assets/www/js/root.js
+++ v3/procApp/platforms/android/app/src/main/assets/www/js/root.js
@@ -1,5 +1,6 @@
 angular.module('root', []).controller("index", ["$scope", "$http", function ($scope, $http) {
     console.log("welcome");
-    $scope.pageTitle = "ProcApp";
+
     $scope.procName = "Tooth Extraction";
+    
 }]);
